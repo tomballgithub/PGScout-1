b6bfa9276f93f2ea320f95e0e5ed2364239eca34
tomballgithub <jeoff@krontz.com>
2018-02-19 10:43:41 -0600

    Update Scout.py

--- pgscout/Scout.py
+++ pgscout/Scout.py
@@ -21,7 +21,7 @@ log = logging.getLogger(__name__)
 NUM_PAUSE_SAMPLES = 60
 
 ENCOUNTER_RESULTS = {
-    0: "ENCOUNTER_ERROR",I 
+    0: "ENCOUNTER_ERROR",
     1: "ENCOUNTER_SUCCESS",
     2: "ENCOUNTER_NOT_FOUND",
     3: "ENCOUNTER_CLOSED",
